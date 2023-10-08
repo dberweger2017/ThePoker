cff0c2080fc0c9720ac3e168c63ad796dc7cb603
Davide Berweger <dberweger2017@gmail.com>
2023-10-08 20:07:54 +0300

    Created connection between client and server

--- client.py
+++ client.py
@@ -1,8 +1,39 @@
 import socket
 
-HOST = 'localhost'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = 'localhost'
+PORT = 8000
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    # TODO: Implement client logic here
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            try:
+                data = s.recv(1024)
+                if data:
+                    decoded_data = data.decode().strip()
+                    if decoded_data.startswith("(I)"):
+                        # Handle (I) data
+                        if decoded_data == "(I)-disconnect":
+                            print("Server has closed the connection.")
+                            s.close()
+                            break
+                        else:
+                            print(data.decode(), end='')
+
+                    elif decoded_data.startswith("(C)"):
+                        # Handle (C) data
+                        print(data.decode(), end='')
+                        response = input("Client choice: ")
+                        s.send(response.encode())
+                    else:
+                        print("Unknown message: ", data.decode(), end="")
+                        
+            except KeyboardInterrupt:
+                print("\nClosing connection...")
+                s.send("(I)-disconnect".encode())
+                s.close()
+                break
+                    
+except ConnectionRefusedError:
+    print('The server is not running.')
+    exit()
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+from Game import Game
+from Player import Player
 
 # Define the host and port to listen on
 HOST = 'localhost'
@@ -10,27 +12,26 @@ def handle_client(conn, addr, client_id):
     print(f'New client connected with ID {client_id}: {addr}')
 
     # Send the client their ID
-    conn.send(f'Your ID is {client_id}\n'.encode())
+    conn.send(f'(I)-Your ID is {client_id}\n'.encode())
+
+    player = Player(client_id, 1000, 0)
 
     # Prompt the client to choose a game
-    conn.send('Choose a game:\n1. Poker\n2. Blackjack\n'.encode())
+    conn.send('(C)-Choose a game:\n1. Poker\n2. Blackjack\n'.encode())
 
     # Wait for the client's response
     while True:
         data = conn.recv(1024)
-        if not data:
-            break
-        response = data.decode().strip()
-        if response == '1':
-            print(f'Client {client_id} chose Poker')
-            conn.send('You chose Poker\n'.encode())
-            break
-        elif response == '2':
-            print(f'Client {client_id} chose Blackjack')
-            conn.send('You chose Blackjack\n'.encode())
-            break
-        else:
-            conn.send('Invalid choice. Please choose again:\n1. Poker\n2. Blackjack\n'.encode())
+        if data:
+            response = data.decode().strip()
+            if response == '1':
+                print(f'Client {client_id} chose Poker')
+                conn.send('You chose Poker\n'.encode())
+            elif response == '2':
+                print(f'Client {client_id} chose Blackjack')
+                conn.send('You chose Blackjack\n'.encode())
+            else:
+                conn.send('Invalid choice. Please choose again:\n1. Poker\n2. Blackjack\n'.encode())
 
     # Close the connection when the client disconnects
     print(f'Client {client_id} disconnected: {addr}')
@@ -49,7 +50,12 @@ print(f'Server listening on {HOST}:{PORT}')
 client_id_counter = 0
 
 # Keep listening for incoming connections and spawn a new thread to handle each one
-while True:
-    conn, addr = server_socket.accept()
-    client_id_counter += 1
-    threading.Thread(target=handle_client, args=(conn, addr, client_id_counter)).start()
+try:
+    while True:
+        conn, addr = server_socket.accept()
+        client_id_counter += 1
+        threading.Thread(target=handle_client, args=(conn, addr, client_id_counter)).start()
+
+except KeyboardInterrupt:
+    print("Server shutting down...")
+    server_socket.close()
