47852db333d410a1db96fffd3076f24c574d4f19
Davide Berweger <dberweger2017@gmail.com>
2023-10-08 20:46:28 +0300

    Printing the stages correctley

--- classes/Game.py
+++ classes/Game.py
@@ -7,10 +7,22 @@ class Game():
         self.deck = Deck()
         self.deck.shuffle()
 
+    def getStage(self):
+        if self.stage == 1:
+            return "Pre-Flop"
+        elif self.stage == 2:
+            return "Flop"
+        elif self.stage == 3:
+            return "Turn"
+        elif self.stage == 4:
+            return "River"
+        else:
+            return "Unknown"
+
     def showDeck(self, showCards = True):
         print(f"Game with {len(self.deck.cards)} cards:")
         if showCards:
             self.deck.show()
     
     def __str__(self):
-        return f"Game with {len(self.players)} players, on stage: {self.stage}"
+        return f"Game with {len(self.players)} players, on stage: {self.getStage()}"
